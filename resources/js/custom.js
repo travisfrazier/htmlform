418b3f65d69c49adf7c6f54d378c56124c537f0b
travisfrazier <travisf777@gmail.com>
2018-03-26 16:16:12 -0400

    js updates

---
+++ resources/js/custom.js
@@ -0,0 +1,36 @@
+// Event listener for user to submit the form
+document.querySelector('form').addEventListener('submit', submitForm);
+
+// Input Variables for form
+function checkFormInputs() {
+  var nameInputField = document.querySelector('#name');
+  var emailInputField = document.querySelector('#mail');
+  var numberInputField = document.querySelector('#number');
+  var cityInputField = document.querySelector('#city');
+  var stateSelect = document.querySelector('#state');
+  var zipInputField = document.querySelector('#zip');
+
+  var values = [nameInputField, emailInputField, numberInputField, cityInputField, stateSelect, zipInputField];
+  var errorFound = false;
+
+  // adding the class 'error' to any input without data
+  for (var i = 0; i < values.length; i++) {
+    var field = values[i];
+    if (!field.value || stateSelect.value === 'Choose State') {
+      field.classList.add('error');
+      errorFound = true;
+    } else {
+      field.classList.remove('error');
+    }
+  }
+
+  // Check if any field had an error, if not, submit data somewhere
+  if (!errorFound) {
+    // save data to databasae
+  }
+}
+
+function submitForm(e) {
+  e.preventDefault(); // prevent defualt action of form refreshing page 
+  checkFormInputs();
+}
